54854ea628dfcc6acdce860f95627b350a3ebfd7
kipstakkr <karthikeyan230396@gmail.com>
2020-02-22 22:08:19 +0530

    Included the solution to the 7th problem in the arrays section. Added test cases accordingly.

---
+++ programming/arrays/max_non_negative_subarray.py
@@ -0,0 +1,68 @@
+"""
+Contains the solution to problem 7 of the array section.
+
+Problem
+-------
+Given an array of integers `A` of length `N`, find out the maximum sum sub-array of non negative
+numbers from A.
+
+The sub-array should be contiguous i.e., a sub-array created by choosing the second and fourth
+element and skipping the third element is invalid. Maximum sub-array is defined as sum of the
+elements in a sub-array.
+
+Note:-
+1.  If there is a tie, then compare with segment's length and return segment which has maximum
+    length.
+2.  If there is still a tie, then return the segment with minimum starting index.
+
+Write a function to return the non-negative sub-array which has the maximum sum.
+
+Constraints
+-----------
+*   1 <= N <= 10 ** 4
+*   -10 ** 4 <= A[i] <= 10 ** 4
+*   0 <= i < N
+
+"""
+from .max_sum_contiguous_subarray import is_non_negative
+
+
+def is_negative(array):  # T(n), S(1)
+    """Check if the given array contains only negative elements."""
+    return all(element < 0 for element in array)
+
+
+def get_max_non_negative_subarray_sum(array):  # T(n), S(1)
+    """Return the non-negative sub-array from the given array which has the maximum sum."""
+    if is_non_negative(array):  # T(n)
+        return array
+
+    elif is_negative(array):  # T(n)
+        return []
+
+    else:
+        max_subarray_sum, max_subarray_length = 0, 0
+        start_index, end_index = 0, 0
+        current_start, current_end = 0, 0
+        current_sum, current_subarray_length = 0, 0
+        for index, element in enumerate(array):  # T(n)
+            if element < 0:
+                current_sum = 0
+                current_start = index + 1
+
+            current_sum += element
+            current_end = index + 1
+
+            # handling tie situation
+            if current_sum == max_subarray_sum:
+                current_subarray_length = current_end - current_start
+                if current_subarray_length > max_subarray_length:
+                    max_subarray_sum = current_sum
+                    start_index, end_index = current_start, current_end
+
+            elif current_sum > max_subarray_sum:
+                max_subarray_sum = current_sum
+                max_subarray_length = current_end - current_start
+                start_index, end_index = current_start, current_end
+
+        return array[start_index: end_index]
---
+++ tests/test_arrays/test_max_non_negative_subarray.py
@@ -0,0 +1,34 @@
+"""Tests for the programming.arrays.max_non_negative_subarray module."""
+
+from hypothesis import given
+from hypothesis.strategies import integers, lists
+
+from programming.arrays.max_non_negative_subarray import (
+    get_max_non_negative_subarray_sum,
+    is_negative
+)
+
+import pytest
+
+
+@given(lists(integers(), min_size=1))
+def test_is_negative(array):
+    """is_negative() should return `True` when all elements are less than zero."""
+    expected_result = not any(element >= 0 for element in array)
+
+    assert is_negative(array) == expected_result
+
+
+@pytest.mark.parametrize(
+    'mock_array, expected_result',
+    [
+        ([1, 2, 5, -7, 2, 3], [1, 2, 5]),
+        ([10, -1, 2, 3, -4, 100], [100]),
+        ([-431, -1341, -3421, -5452], []),
+        ([1232, 4531, 1245, 1243, 9327, 9474], [1232, 4531, 1245, 1243, 9327, 9474]),
+        ([-432] + [10 ** 3] * 10 ** 3 + [-836, 10 ** 4], [10 ** 3] * 10 ** 3)
+    ]
+)
+def test_get_max_non_negative_subarray_sum(mock_array, expected_result):
+    """get_max_non_negative_subarray_sum() should return the maximum non-negative subarray."""
+    assert get_max_non_negative_subarray_sum(mock_array) == expected_result
